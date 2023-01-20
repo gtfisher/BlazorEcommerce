34ab7d14b2eae7cea730982f93ee0f254c2efa5e
Grant TF <grant@tarrant-fisher.co.uk>
2023-01-20 09:06:46 +0000

    Product Details, Categroeis & Seeding more products

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -18,6 +18,10 @@ namespace BlazorEcommerce.Shared
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        public Category? Category { get; set; }
+
+        public int CategoryID { get; set; }
+
 
     }
 }
